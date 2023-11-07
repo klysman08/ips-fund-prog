c3c15953e57a209a09d8c11a4d10d4824d5f2e08
klysman08 <klysman.rk@gmail.com>
2023-11-07 01:38:45 +0000

    Add function to estimate pi

--- LAB4/4_estimando_pi.py
+++ LAB4/4_estimando_pi.py
@@ -3,9 +3,28 @@ File: 4_estimando_pi.py
 ----------------------
 indique o que faz este programa
 """
+import random
 
 def main():
-    pass
+    print(f'PI estimado: {estima_pi()}')
+    
+NUM_PONTOS = 100000
+
+def gera_pontos(num_pontos):
+    soma=0
+    for i in range(num_pontos):
+        x=random.uniform(-1,1)
+        y=random.uniform(-1,1)
+        i=i+1
+        if x**2+y**2<=1:
+            soma=soma+1
+    return soma
+    
+def estima_pi(pontos=gera_pontos(NUM_PONTOS)):
+    print(f'Total de pontos somados: {pontos}')
+    pi=4*pontos/NUM_PONTOS
+    return pi
+
 
 
 # Esta linha fornecida é necessária no final de um ficheiro Python
